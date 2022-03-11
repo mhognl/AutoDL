0edd7afa0e811e1fe42edacd572dea0ec6e0049b
mhognl <43824816+mhognl@users.noreply.github.com>
2022-03-11 09:56:23 +0100

    AutoDL V2

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ ! "`whoami`" = "root" ]
+then
+	echo "You must run this script as root."
+	exit 1
+fi
+echo "Installing AutoDL now..."
+apt update &&
+apt full-upgrade -y &&
+apt install curl ffmpeg wget acl -y &&
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+wget https://github.com/mhognl/AutoDL/raw/master/autodl -O /usr/local/bin/autodl
+chmod a+rx /usr/local/bin/autodl
+mkdir -p /usr/local/src/ytdownloads
+touch /usr/local/src/ytdownloads/list.txt
+setfacl -R -m u::rwx /usr/local/src/ytdownloads
+setfacl -R -m g::rwx /usr/local/src/ytdownloads
+setfacl -R -m o::rwx /usr/local/src/ytdownloads
+echo "You can add music by applying links from YouTube to /usr/local/src/ytdownloads/list.txt"
+echo "Finished!"
+echo "Now you can run the command 'autodl' as normal user!"
